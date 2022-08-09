ad43cc491e95832b5ebd320f6221238be7e0f901
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-08-09 15:01:33 -0300

    @RafaelCandeias00 [Task: criação e configuração de modelo e contexto concluido] {09/08/2022 15:01}

--- SolucaoAmina/AminaApi/Startup.cs
+++ SolucaoAmina/AminaApi/Startup.cs
@@ -26,7 +26,7 @@ namespace AminaApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AminaContexto>(opt => opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
+            services.AddDbContext<AminaContextos>(opt => opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
             services.AddControllers();
         }
 
