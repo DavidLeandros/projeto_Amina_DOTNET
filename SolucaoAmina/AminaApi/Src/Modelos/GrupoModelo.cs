e38e3630ccb389c81f4b4eeb05a7635077dff320
Sabryna <sabrynadeangelo@gmail.com>
2022-08-09 12:11:07 -0300

    @Sabrynad [Task tirando projeto DOTNET]{09/08/2022 12:10}

--- SolucaoAmina/AminaApi/Src/Modelos/GrupoModelo.cs
+++ SolucaoAmina/AminaApi/Src/Modelos/GrupoModelo.cs
@@ -1,6 +1,30 @@
-﻿namespace AminaApi.Src.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AminaApi.Src.Modelos
 {
     public class GrupoModelo
     {
+        [Table("tb_grupo")]
+        public class Tema
+        {
+            #region Atributos
+
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+            public int Id { get; set; }
+
+            public string Titulo { get; set; }
+
+            public string Descricao { get; set; }
+
+            public string Topico { get; set; }
+
+            public string Midia { get; set; }
+
+
+            #endregion
+        }
     }
 }
