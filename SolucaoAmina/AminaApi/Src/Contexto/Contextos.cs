f7bb2cb14a24e83eb5806666fa820969b09187c0
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-08-09 14:04:21 -0300

    @RafaelCandeias00 [Task: configurando contexto] {09/08/2022 14:04}

--- SolucaoAmina/AminaApi/Src/Contexto/Contextos.cs
+++ SolucaoAmina/AminaApi/Src/Contexto/Contextos.cs
@@ -1,6 +1,20 @@
-﻿namespace AminaApi.Src.Contexto
+﻿using AminaApi.Src.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace AminaApi.Src.Contexto
 {
-    public class Contextos
+    public class AminaContextos : DbContext
     {
+        #region Atributos
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Postagem> Postagens { get; set; }
+        public DbSet<Grupo> Grupos { get; set; }
+        #endregion
+
+        #region
+        public AminaContextos(DbContextOptions<AminaContextos> opt) : base(opt)
+        {
+        }
+        #endregion
     }
 }
