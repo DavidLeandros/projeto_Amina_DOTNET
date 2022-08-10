92a71179a83d8891a7f50abfa4b8077e0ea6b344
Samira Pessoal <sixoobecan@gmail.com>
2022-08-10 15:29:45 -0300

    [Add Interface e Repositorio de Postagem]{10/08/2022 15:29}

--- SolucaoAmina/AminaApi/Src/Repositorios/IPostagem.cs
+++ SolucaoAmina/AminaApi/Src/Repositorios/IPostagem.cs
@@ -1,6 +1,16 @@
-﻿namespace AminaApi.Src.Repositorios
+﻿using AminaApi.Src.Modelos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AminaApi.Src.Repositorios
 {
     public interface IPostagem
     {
+        Task<List<Postagem>> PegarTodasPostagemAsync();
+        Task<Postagem> PegarPostagensPeloIdAsync(int id);
+        Task NovaPostegemAsync(Postagem postagem);
+        Task AtualizarPostagemAsync(Postagem postagem);
+        Task DeletarPostagemAsync(int id);
+
     }
 }
--- SolucaoAmina/AminaApi/Src/Repositorios/Implementacoes/PostagemRepositorio.cs
+++ SolucaoAmina/AminaApi/Src/Repositorios/Implementacoes/PostagemRepositorio.cs
@@ -1,6 +1,85 @@
-﻿namespace AminaApi.Src.Repositorios.Implementacoes
+﻿using AminaApi.Src.Contexto;
+using AminaApi.Src.Modelos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AminaApi.Src.Repositorios.Implementacoes
 {
-    public class PostagemRepositorio
+    public class PostagemRepositorio : IPostagem
     {
+        #region Atributos
+        private readonly AminaContextos _contexto;
+        #endregion
+
+        #region Construtores
+        public PostagemRepositorio(AminaContextos contextos) 
+        { 
+            _contexto = contextos;
+        }
+        #endregion
+
+        #region Método
+        public Task AtualizarPostagemAsync(Postagem postagem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task DeletarPostagemAsync(int id)
+        {
+            _contexto.Postagens.Remove(await PegarPostagensPeloIdAsync(id));
+            await _contexto.SaveChangesAsync();
+
+        }
+
+        public async Task NovaPostegemAsync(Postagem postagem)
+        {
+
+            if (!ExisteIdUsuario(postagem.Usuario.Id)) throw new Exception("id do Usuario não encontrado");
+
+            if (!ExisteIdGrupo(postagem.Grupo.Id)) throw new Exception("Grupo não encontrado");
+
+            await _contexto.Postagens.AddAsync(new Postagem
+            {
+                Titulo = postagem.Titulo,
+                Descricao = postagem.Descricao,
+                Foto = postagem.Foto,
+                Usuario = _contexto.Usuarios.FirstOrDefault(p => p.Id == postagem.Usuario.Id),
+                Grupo = _contexto.Grupos.FirstOrDefault(g => g.Id == postagem.Grupo.Id)
+            });
+            await _contexto.SaveChangesAsync();
+           
+            //função auxiliar
+            bool ExisteIdUsuario(int id)
+            {
+                var auxiliar = _contexto.Usuarios.FirstOrDefault(p => p.Id == id);
+
+                return auxiliar != null;
+            }
+
+            bool ExisteIdGrupo(int id)
+            {
+                var auxiliar = _contexto.Grupos.FirstOrDefault(g => g.Id == id);
+
+                return auxiliar != null;
+            }
+        }
+        
+        public Task<Postagem> PegarPostagensPeloIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<Postagem>> PegarTodasPostagemAsync()
+        {
+            return await _contexto.Postagens
+                .Include(u => u.Usuario)
+                .Include(g => g.Grupo)
+                .ToListAsync();
+        }
+                
+        #endregion
     }
 }
