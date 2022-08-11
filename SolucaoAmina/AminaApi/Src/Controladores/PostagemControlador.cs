31c7a5b2fb41d00a1d3821f81bed8aec310f3348
rodrigox98 <rodrigo.silva31@hotmail.com>
2022-08-11 14:34:06 -0300

    @rodrigox98 [Task: Adicionando get no controlador]{ 11/08/2022 14:33}

--- SolucaoAmina/AminaApi/Src/Controladores/PostagemControlador.cs
+++ SolucaoAmina/AminaApi/Src/Controladores/PostagemControlador.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AminaApi.Src.Repositorios;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AminaApi.Src.Controladores
 {
-    [Route("api/[controller]")]
+    [Route("api/Postagens")]
     [ApiController]
+    [Produces("application/json")]
     public class PostagemControlador : ControllerBase
     {
+        #region Atributos
+        private readonly IPostagem _repositorio;
+        #endregion
+
+        #region Construtor
+        public PostagemControlador(IPostagem repositorio)
+        {
+            _repositorio = repositorio;
+        }
+        #endregion
+
+        #region Metodos
+        [HttpGet]
+        public async Task<ActionResult> PegarTodasPostagensAsync()
+        {
+            var lista = await _repositorio.PegarTodasPostagemAsync();
+
+            if (lista.Count < 1) return NoContent();
+
+            return Ok(lista);
+        }
+
+        #endregion
     }
 }
