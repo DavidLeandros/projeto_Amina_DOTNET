0c63eb7ef2856d86a68d2269f2c6edc60beaff66
Maju França <juliafkoch@gmail.com>
2022-08-22 12:04:34 -0300

    @majufrnc [Task: Add Swagger] {22/08/2022 12h04}

--- SolucaoAmina/AminaApi/Startup.cs
+++ SolucaoAmina/AminaApi/Startup.cs
@@ -18,6 +18,9 @@ using AminaApi.Src.Servicos;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
 
 namespace AminaApi
 {
@@ -64,17 +67,56 @@ namespace AminaApi
                     ValidateIssuer = false,
                     ValidateAudience = false
                 };
-            }
-            );
+            });
+            services.AddSwaggerGen(
+                s =>
+                {
+                    s.SwaggerDoc("v1", new OpenApiInfo { Title = "Blog Pessoal", Version = "v1" });
+                    s.AddSecurityDefinition(
+                        "Bearer",
+                        new OpenApiSecurityScheme()
+                        {
+                            Name = "Authorization",
+                            Type = SecuritySchemeType.ApiKey,
+                            Scheme = "Bearer",
+                            BearerFormat = "JWT",
+                            In = ParameterLocation.Header,
+                            Description = "JWT authorization header utiliza: Bearer + JWTToken",
+                        });
+
+                    s.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new List<string>()
+                        }
+                    });
+
+                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                    s.IncludeXmlComments(xmlPath);
+                });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AminaContextos contexto)
         {
             if (env.IsDevelopment())
             {
                 contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogPessoal v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
             //Ambiente de produção
 
@@ -83,7 +125,7 @@ namespace AminaApi
 
             app.UseRouting();
 
-            app.UseCors(c => c 
+            app.UseCors(c => c
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
                 .AllowAnyHeader()
@@ -99,3 +141,4 @@ namespace AminaApi
         }
     }
 }
+
